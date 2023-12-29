9995b550aad792b694c63e055ba919f7e460f0c2
Ananya Bhardwaj <97379193+Ananya-Bhardwaj@users.noreply.github.com>
2023-12-29 17:46:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose'; 
+import dotenv from 'dotenv'; 
+dotenv.config(); 
+
+import userRoute from './routes/user.js'; 
+import postsRoute from './routes/posts.js'; 
+import commentsRoute from './routes/comments.js'; 
+import blogRoute from './routes/blog.js'; 
+
+const app = express();
+app.use(express.json()); 
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    console.log(error); 
+  }
+};
+
+connect();
+
+app.use('/api/user' ,userRoute); 
+app.use('/api/posts', postsRoute); 
+app.use('/api/comments', commentsRoute); 
+app.use('/api/blog', blogRoute); 
+
+app.listen(5500, () => {
+  console.log('Connected to backend, listening on port 5500');
+});
+
