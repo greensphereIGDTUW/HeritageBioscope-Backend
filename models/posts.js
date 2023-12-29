4d38dfbf4c369cabfd9f7f3c138de367ec25fff5
Ananya Bhardwaj <97379193+Ananya-Bhardwaj@users.noreply.github.com>
2023-12-29 17:45:30 +0530

    Added the models/schema

---
+++ models/posts.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const PostsSchema = new mongoose.Schema(
+    {
+        Title: {
+            type: String,
+            required: true
+        },
+        Author: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+        },
+        Content: {
+            type: String,
+            required: true,
+        }, 
+        Photo: {
+            type: String,
+        },
+        num_likes: {
+            type: Number,
+
+        },
+        num_dislikes: {
+          type: Number,
+
+      }
+      // comments: [
+      //     {
+      //       comment_id:{
+      //       type: mongoose.Schema.Types.ObjectId,
+      //       ref: 'Comments',
+      //       default: null
+      //       }
+      //   }
+      //   ],
+    }, 
+  {timestamps: true}
+);
+
+export default mongoose.model("Posts", PostsSchema);
